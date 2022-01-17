748428db86b106ab50adf72a33bd367ade202867
666Parzival666 <oven.igor@yandex.ru>
2022-01-17 12:40:34 +0300

    LabRab4.1

--- LabRab4.1_CString/LabRab4.1_CString.cpp
+++ LabRab4.1_CString/LabRab4.1_CString.cpp
@@ -43,23 +43,22 @@ void task2()
 	}
 	cout << "Подстрока '" << ss << "' встречается в строке '" << s << "' " << n << " раз" << endl;
 }
-void task3() 
+void encrypt(char* str_for_encrypt, int key)
 {
-	int k;
-	char buff[50];
-	cout << "Введите сообщение : ";
-	cin >> buff;
-	cout << "Введите кол-во букв для сдвига : ";
-	cin >> k;
-	for (int i = 0; i < strlen(buff); i++)
-		buff[i] += k;
-	cout << "Шифр : " << buff << endl;
-
-	cout << "Расшифровка : ";
-	for (int i = 0; i < strlen(buff); i++)
-		buff[i] -= k;
-
-	cout << buff << endl;
+	for (int i = 0; i < strlen(str_for_encrypt); i++) {
+		if ('a' <= str_for_encrypt[i] && str_for_encrypt[i] <= 'z') {
+			if (str_for_encrypt[i] + key > 'z') // Если происходит выход за пределы алфавита
+				str_for_encrypt[i] = 'z' - str_for_encrypt[i] + 'a' + key - 1; // Переносимся в начало и применяем ключ
+			else str_for_encrypt[i] += key;
+		}
+		else if ('A' <= str_for_encrypt[i] && str_for_encrypt[i] <= 'Z') { // Тот же принцип, но для верхнего регистра
+			if (str_for_encrypt[i] + key > 'Z')
+				str_for_encrypt[i] = 'Z' - str_for_encrypt[i] + 'A' + key - 1;
+			else str_for_encrypt[i] += key;
+		}
+		else if (str_for_encrypt[i] == ' ');
+	}
+	return;
 }
 void task4() 
 {
@@ -90,11 +89,11 @@ int main()
 	int choice = 0;
 	while (true) 
 	{
-		cout << "1.Задание 1" << endl;
-		cout << "2.Задание 2" << endl;
-		cout << "3.Задание 3" << endl;
-		cout << "4.Задание 4" << endl;
-		cout << "5.Выход" << endl;
+		cout << "№1.Палиндром" << endl;
+		cout << "№2.Поиск подстроки" << endl;
+		cout << "№3.Шифр Цезаря" << endl;
+		cout << "№4.Найти слова в кавычках" << endl;
+		cout << "№5.Выход" << endl;
 		cin >> choice;
 		switch (choice)
 		{
@@ -110,7 +109,18 @@ int main()
 			}
 			case 3: 
 			{
-				task3();
+				int key;
+				char string[255];
+
+				cout << "Введите текст: "; cin.ignore(); cin.getline(string, 255);
+				cout << "Введите ключ: "; cin >> key;
+
+				encrypt(string, key);
+				char* str_for_encrypt = new char[strlen(string) + 1];
+				str_for_encrypt = string + '\0';
+				cout << "Encrypted string: " << str_for_encrypt << endl;
+				cout << endl;
+
 				break;
 			}
 			case 4: 
