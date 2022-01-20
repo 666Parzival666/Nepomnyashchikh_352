929bf6636f2e643c108a2b061a728ee408911a8a
666Parzival666 <oven.igor@yandex.ru>
2022-01-20 17:16:10 +0300

    LabRab6.6

---
+++ LabRab6.6/LabRab6.6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <map>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string input;
+    map <string, int> map1;
+
+    double count = 0;
+    int j = 1;
+    cout << "Введите текст: ";
+    while (cin >> input)
+    {
+        count++;
+        if (map1.find(input) == map1.end())
+        {
+            map1.insert(pair<string, int>(input, j));
+        }
+        else
+        {
+            map1[input] += 1;
+        }
+    }
+
+    for (auto i = map1.begin(); i != map1.end(); i++)
+    {
+        cout << i->first << " " << (double(i->second / count) * 100) << "%" << endl;
+    }
+}
