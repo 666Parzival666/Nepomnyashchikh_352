777c12a032bfadc2b4646ef93e593eeaac80a8dc
666Parzival666 <oven.igor@yandex.ru>
2022-01-20 10:07:35 +0300

    LabRab6.4

---
+++ LabRab6.4/LabRab6.4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstring>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	set <string> test;
+	string input;
+	while (true)
+	{
+		cout << "Введите сообщение: "; cin >> input;
+
+		if (test.find(input) == test.end())
+		{
+			cout << "Сообщение отправленно!" << endl;
+			cout << endl;
+			test.insert(input);
+		}
+
+		else
+		{
+			cout << "Произошла ошибка" << endl; "\n";
+			cout << endl;
+		}
+	}
+}
